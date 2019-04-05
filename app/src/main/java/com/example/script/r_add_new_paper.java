433d0d3297228ab7ff0429f5914a90b1298d93fe
Warda <wmsays@gmail.com>
2019-04-05 21:00:54 +0500

    initial commit

---
+++ app/src/main/java/com/example/script/r_add_new_paper.java
@@ -0,0 +1,13 @@
+package com.example.script;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class r_add_new_paper extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_r_add_new_paper);
+    }
+}
